6119d9d36ff0a55c325ec5cd7902d2ffa141183f
sorki <srk@48.io>
2024-03-31 17:33:35 +0200

    todo candump -e like

--- app/HCANDump.hs
+++ app/HCANDump.hs
@@ -11,3 +11,9 @@ main = do
   void $ runSocketCAN "vcan0" $ do
     forever $ recv >>= liftIO . print
 
+-- needs Network.CAN.Pretty or Builder or smthing
+-- that does the same ID formatting as SLCAN.Builder:78
+-- a la
+-- $ candump -e vcan0
+--  vcan0  001237E5   [2]  4C EE
+--  vcan0       7E5   [2]  4C EE
