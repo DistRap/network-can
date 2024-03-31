9b7c10d2d585cffe5f3c0391eb7d9219ca391ee6
sorki <srk@48.io>
2024-03-31 20:04:59 +0200

    ahm

---
+++ app/HCANBridge.hs
@@ -0,0 +1,17 @@
+module Main where
+
+import Control.Concurrent.Classy.Async
+import Control.Monad (forever, void)
+import Control.Monad.IO.Class (MonadIO(liftIO))
+import Control.Monad.Trans (lift)
+import Data.Default.Class
+import Network.CAN
+import Network.SLCAN
+import Network.SocketCAN
+
+main :: IO () -- Either CANError ())
+main = do
+  void $ runSLCANFilePath "/tmp/ttyV1" def $ do
+     void $ runSocketCAN "vcan0" $ do
+        (forever $ lift recv >>= send)
+        (forever $ recv >>= lift . send)
