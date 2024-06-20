79bf769c9453bd844eb232a0084da36ab546af03
sorki <srk@48.io>
2024-06-20 19:35:09 +0200

    fix bridge using serialport

--- app/CANBridge.hs
+++ app/CANBridge.hs
@@ -3,11 +3,13 @@ module Main where
 
 import Control.Monad.Trans (MonadTrans(lift))
 import Data.Default.Class (Default(def))
+import System.Hardware.Serialport (CommSpeed(..), SerialPortSettings(..))
 
 import qualified Control.Monad
 import qualified Network.CAN
 import qualified Network.SocketCAN
 import qualified Network.SLCAN
+import qualified System.Hardware.Serialport
 import qualified System.IO
 
 import qualified UnliftIO.Async
@@ -19,13 +21,17 @@ import qualified UnliftIO.Async
 -- ./build/canopen-posix-test/tower_init <> /tmp/ttyV1  > /tmp/ttyV1
 main :: IO ()
 main = do
-  System.IO.withFile "/tmp/ttyV0" System.IO.ReadWriteMode $ \h ->
-    Network.SLCAN.runSLCAN h def $ do
-      Network.SocketCAN.runSocketCAN "vcan0" $ do
-          UnliftIO.Async.race_
-            (Control.Monad.forever
-             $ Network.CAN.recv >>= lift . Network.CAN.send
-            )
-            (Control.Monad.forever
-             $ lift Network.CAN.recv >>= Network.CAN.send
-            )
+  h <- System.Hardware.Serialport.hOpenSerial
+    "/dev/can4discouart"
+    (System.Hardware.Serialport.defaultSerialSettings
+      { commSpeed = CS115200 }
+    )
+  Network.SLCAN.runSLCAN h def $ do
+    Network.SocketCAN.runSocketCAN "vcan0" $ do
+        UnliftIO.Async.race_
+          (Control.Monad.forever
+           $ Network.CAN.recv >>= lift . Network.CAN.send
+          )
+          (Control.Monad.forever
+           $ lift Network.CAN.recv >>= Network.CAN.send
+          )
