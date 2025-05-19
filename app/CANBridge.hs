8410f534f3e67bf363569ada14863d27a7a4017c
sorki <srk@48.io>
2025-05-19 16:36:59 +0200

    Cleanup CANBridge

--- app/CANBridge.hs
+++ app/CANBridge.hs
@@ -3,35 +3,30 @@ module Main where
 
 import Control.Monad.Trans (MonadTrans(lift))
 import Data.Default.Class (Default(def))
-<<<<<<< HEAD
-import System.Hardware.Serialport (CommSpeed(..), SerialPortSettings(..))
-=======
 import Network.SLCAN (Transport(..))
->>>>>>> 759c2cd (Abstract Transport for SLCAN allowing UDP transport as well)
+import System.Hardware.Serialport (CommSpeed(..), SerialPortSettings(..))
 
 import qualified Control.Monad
 import qualified Network.CAN
 import qualified Network.SocketCAN
 import qualified Network.SLCAN
 import qualified System.Hardware.Serialport
-import qualified System.IO
-
 import qualified UnliftIO.Async
 
--- bridge vcan0 to virtual /tmp/ttyV0 serial port
--- created with
--- socat -d -d pty,link=/tmp/ttyV0,raw,echo=0 pty,link=/tmp/ttyV1,raw,echo=0
--- so we can run ivory-tower-posix against it
--- ./build/canopen-posix-test/tower_init <> /tmp/ttyV1  > /tmp/ttyV1
+-- | Bridge vcan0 to slcan over /dev/can4discouart serial port
 main :: IO ()
 main = do
-  System.IO.withFile "/tmp/ttyV0" System.IO.ReadWriteMode $ \h ->
-    Network.SLCAN.runSLCAN (Transport_Handle h) def $ do
-      Network.SocketCAN.runSocketCAN (Network.SocketCAN.mkCANInterface "vcan0") $ do
-          UnliftIO.Async.race_
-            (Control.Monad.forever
-             $ Network.CAN.recv >>= lift . Network.CAN.send
-            )
-            (Control.Monad.forever
-             $ lift Network.CAN.recv >>= Network.CAN.send
-            )
+  h <- System.Hardware.Serialport.hOpenSerial
+    "/dev/can4discouart"
+    (System.Hardware.Serialport.defaultSerialSettings
+      { commSpeed = CS115200 }
+    )
+  Network.SLCAN.runSLCAN (Transport_Handle h) def $ do
+    Network.SocketCAN.runSocketCAN (Network.SocketCAN.mkCANInterface "vcan0") $ do
+        UnliftIO.Async.race_
+          (Control.Monad.forever
+           $ Network.CAN.recv >>= lift . Network.CAN.send
+          )
+          (Control.Monad.forever
+           $ lift Network.CAN.recv >>= Network.CAN.send
+          )
